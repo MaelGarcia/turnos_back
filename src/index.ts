ab2c6e6801d5723fde8c410a5e0c8bde5ef0db82
Edwin Garcia <edwinch.gar@gmail.com>
2025-12-10 11:37:33 -0600

    update index.ts view port db

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,18 @@ process.env.TZ = 'America/Mexico_City';
 import { createServer } from 'http';
 import { buildServer } from './server/app';
 import { initSocket } from './tools/socket';
-import { startBot } from '../src/bot/main';
+import { startBot } from './bot/main';
 
 const app = buildServer();
 const server = createServer(app);
 const EXPRESS_PORT = process.env.EXPRESS_PORT;
+const DB_PORT = process.env.POSTGRES_DB_PORT;
 
 // Inicializar Socket.IO
 initSocket(server);
 
 server.listen(EXPRESS_PORT, () => {
+  console.log(`📀 Base de datos conectando atraves del puerto: ${DB_PORT}`)
   console.log(`🌐 Servidor API con Socket.IO en http://localhost:${EXPRESS_PORT}`);
 });
 
